75d41cfead5ca0721f29cfbe70f758d077967635
John Lavik <johnrlavik@gmail.com>
2021-08-24 15:49:35 -0500

    Piston in progress

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.DriveForwardTimed;
+import frc.robot.commands.DriveWithJoysticks;
+import frc.robot.commands.ShootBall;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Shooter;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  //Drivetrain declare
+  private final DriveTrain driveTrain;
+  private final DriveWithJoysticks driveWithJoysticks;
+  private final DriveForwardTimed driveForwardTimed;
+  public static Joystick driverOldStyleJoystick;
+  public static XboxController driverJoystick;
+  //Shooter declare
+  private final Shooter shooter;
+  private final ShootBall shootBall;
+  
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    driveTrain = new DriveTrain();
+    driveWithJoysticks = new DriveWithJoysticks(driveTrain);
+    driveWithJoysticks.addRequirements(driveTrain);
+    driveTrain.setDefaultCommand(driveWithJoysticks);
+
+    driveForwardTimed = new DriveForwardTimed(driveTrain);
+    driveForwardTimed.addRequirements(driveTrain);
+
+    driverOldStyleJoystick = new Joystick(Constants.JOYSTICK1_NUMBER);
+    driverJoystick = new XboxController(Constants.JOYSTICK_NUMBER);
+
+    shooter = new Shooter();
+    shootBall = new ShootBall(shooter);
+    shootBall.addRequirements(shooter);
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    JoystickButton shootButton = new JoystickButton(driverJoystick, XboxController.Button.kBumperRight.value);
+    shootButton.whileHeld(new ShootBall(shooter));
+
+    
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return driveForwardTimed;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Piston.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Piston extends SubsystemBase {
+  DoubleSolenoid piston1;
+
+  /** Creates a new Piston. */
+  public Piston() {
+    piston1 = new DoubleSolenoid(Constants.PISTON1_FORWARD, Constants.PISTON1_REVERSE);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
